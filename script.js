482c8bafd55dddf20490f70cd8f2b69472e1ab47
al4n1 <164919407+al4n1@users.noreply.github.com>
2025-08-06 13:29:34 -0300

    yaysdguyf

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ function inserirNome(){
 }
 const lista = document.querySelector('#lista');
 
- 
 lista.textContent = linguagens[0];
 lista.textContent = linguagens[1];
 lista.textContent = linguagens[2];
 
-let aluno1 = {
-    mome:'Alan',
-    idade:'17',
-    anoLetivo:'Ensino Medio',
-    materiasFavoritas: ['educaçãoFisica','programação','arte']
-    }
-     console. log(aluno1.nome);
-      console. log(aluno1.idade);
-       console. log(aluno1.materiasFavoritas);   
-        console. log(aluno1.materiasFavoritas[1]);
 
-        const.objeto = document.querySelector('#aluno');
-         objeto.contextContent = aluno1.nome;
-         objeto.contextContent = aluno1.idade;
-         objeto.contextContent = aluno1.aluno1.materiasFavoritas[1];
+let aluno1 ={
+  nome:'Alan',
+  idade: 17,
+  anoletivo:'Esino Médio',
+  materiasFavoritas:['Educação Física e Programação']
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector('#aluno');
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
